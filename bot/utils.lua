e6f410c7fb0ce9ae1c915ca6fb95dda83371bde8
sina1903 <mi.sina.sn@gmail.com>
2017-04-20 17:25:02 +0430

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -1,4 +1,4 @@
---Begin Utils.lua By #BeyondTeam :)
+--Begin Utils.lua By #TG_plus :)
 function serialize_to_file(data, file, uglify)
   file = io.open(file, 'w+')
   local serialized
@@ -71,7 +71,7 @@ end
 --  Saves file to /tmp/. If file_name isn't provided,
 -- will get the text after the last "/" for filename
 -- do ski
-BDRpm = '\n@BeyondTeam'
+BDRpm = '\n@TG_plus'
 -- Waiting for ski:)
 -- and content-type for extension
 function download_to_file(url, file_name)
