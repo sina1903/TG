b9259265322262215ddd47b4e71149de9609f47b
sina1903 <mi.sina.sn@gmail.com>
2017-04-26 13:59:39 +0430

    Tgplus
    Sina

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 157059515 -- put Your ID here! <===
+local SUDO = 373670309 -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
